e61d4491db431e2ad9d58170b5f4d32857353583
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-09-17 14:37:03 -0300

    adicionar os arquivos do projeto

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+import Icon from './assets/images/icon-dice.svg'
+import DividerDesktop from './assets/images/pattern-divider-desktop.svg'
+import DividerMobile from './assets/images/pattern-divider-mobile.svg'
+import api from './services/api'
+
+function App() {
+  const [conselho, setConselho] = useState({})
+
+
+  async function adicionarConselho() {
+    
+    try {
+      const respoonse = await api.get()
+      const data = respoonse.data
+      setConselho(data)
+    }
+    catch {
+      alert('[Erro] conselho não encontrado')
+    }
+  }
+
+  return (
+    <main className='conteudo-principal'>
+      <div className='container'>
+        <h4 className='title'>Advice</h4>
+        {
+          Object.keys(conselho).length > 0 && (
+            <p className='advice animate__animated animate__rubberBand'>{conselho.slip.advice}</p>
+          )
+        }
+        <picture>
+          <source media="(max-width: 450px)" srcset={DividerMobile}/>
+          <img className='line' src={DividerDesktop} alt="linha horizontal"/>
+        </picture>
+        <button onClick={adicionarConselho} className='btn-conselho'>
+          <img src={Icon} alt="icone conselho" />
+        </button>
+      </div>
+    </main>
+  )
+}
+
+export default App
